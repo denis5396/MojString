477e0a842051972553f655fd2aeff449a78e5eca
Denis <denis5396@gmail.com>
2019-06-05 15:43:36 +0200

    xD

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,24 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+
+        //MojString obj = new MojString('d',5);
+        MojString obj = new MojString("Ovo je neki niz xd");
+        System.out.print(obj.toString());
+        System.out.print(obj.size());
+        System.out.print(obj.length());
+        //System.out.print(obj.dodajNaString("dsds"));
+        System.out.println("");
+        MojString obj1 = new MojString("Ovo je neki niz");
+        System.out.println("");
+        //System.out.print(obj1.substring(3,9));
+        System.out.println("");
+        System.out.println("");
+        //System.out.print(obj.equals("Ovo je neki niz"));
+        System.out.print(obj.wordCount());
+        System.out.print(obj.longestWord());
+        System.out.print(obj.shortestWord());
+        System.out.println("");
+        System.out.print(obj1.izbaciString("Ovo je n"));
     }
 }
--- src/MojString.java
+++ src/MojString.java
@@ -1,5 +1,10 @@
 public class MojString {
     private char niz[];
+    private boolean jeLiSlovo(char c){
+        if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9')return true;
+        else{
+            return false;}
+    }
 
 
 
@@ -11,8 +16,13 @@ public class MojString {
     //konstruktor  public MojString(String nekiString);
     @Override
     public String toString(){
+        String rezultat = "";
+        for(int i = 0; i < this.niz.length; i++){
+            rezultat += this.niz[i];
+        }
+
         //Funkcija treba da niz pretvori u string i to vrati
-        return "";
+        return rezultat;
     }
 
 
@@ -26,26 +36,91 @@ public class MojString {
          */
     }
     public MojString(String nekiString){
+        char niz[] = new char[50];
+        int brEl = 0;
+
+        for(int i = 0; i < nekiString.length(); i++){
+            niz[brEl] = nekiString.charAt(i);
+            System.out.print(niz[brEl]);
+            brEl++;
+        }
+        char nizi[] = new char[brEl];
+        for(int i = 0; i < brEl; i++){
+            nizi[i] = niz[i];
+        }
+
+        this.niz = nizi;
+
+
        /*ovaj konstruktor treba da napravi string(niz) koji je identican poslanom stringu*/
     }
     public MojString(char c, int x){
        /*ovaj konstruktor treba da kreira string koji se sastoji od znaka c ponovljenog x puta, tj.
        ako su funckiji poslani parametri 'T' i 5, konstruktor treba da kreira string(niz znakova) "TTTTT"
         */
+       char niz[] = new char[50];
+       int brEl = 0;
+       for(int i = 0; i < x; i++){
+           niz[brEl] = c;
+           System.out.print(niz[brEl]);
+           brEl++;
+       }
+        char nizi[] = new char[brEl];
+        for(int i = 0; i < brEl; i++){
+            nizi[i] = niz[i];
+        }
+
+        this.niz = nizi;
+
+
     }
     public int size(){
         //vraca broj znakova u stringu
-        return 0; //izbrisati ovu liniju koda
+        int count = 0;
+        for(int i = 0; i < this.niz.length; i++){
+            count++;
+        }
+
+        return count; //izbrisati ovu liniju koda
     }
 
     public int length(){
         //vraca broj znakova u stringu
-        return size();
+        int count = 0;
+        for(int i = 0; i < this.niz.length; i++){
+            count++;
+        }
+
+        return count;
     }
 
 
     public MojString dodajNaString(String nekiString){
         // na postojeci string(tj. niz znakova) dodaje string koji je poslan kao argument
+
+        char niz[] = new char[this.niz.length + nekiString.length()];
+        char nizs[] = new char[nekiString.length()];
+        int brEl = 0;
+
+        for(int i = 0; i < nizs.length; i++){
+            nizs[i] = nekiString.charAt(i);
+        }
+
+        for(int i = 0; i < niz.length; i++) {
+            if (i >= this.niz.length) {
+                niz[i] = nizs[brEl];
+                brEl++;
+            }
+            else if(i <= this.niz.length){
+            niz[i] = this.niz[i];
+            }
+        }
+        for(int i = 0; i < niz.length; i++){
+            System.out.print(niz[i]);
+        }
+
+        this.niz = niz;
+
         return this; //vraca referencu na trenutni objekat radi kaskadnog pozivanja
         /*
         Npr.
@@ -54,12 +129,26 @@ public class MojString {
         */
     }
 
-    public int wordCount(){
+    public int wordCount() {
         //Funkcija kao rezultat vraca broj rijeci u stringu.
         //rijec posmatramo kao bilo koju kombinaciju slova i brojeva, npr.
         // asd, as123dada, 123 su 3 rijeci.
-        return 0;//izbrisati ovu liniju koda
+        int count = 0;
+        int i = 0;
+        while(i < this.niz.length){
+
+            while(i < this.niz.length && jeLiSlovo(this.niz[i])){
+                i++;
+            }
+            count++;
+
+            while(i < this.niz.length && !jeLiSlovo(this.niz[i])){
+                i++;
+            }
+        }
+            return count;//izbrisati ovu liniju koda
     }
+
 
     public String substring(int pocetniIndeks, int krajnjiIndeks){
         //funkcija kao rezultat vraca dio stringa sa pocetkom i krajem definisanim u parametrima funkcije
@@ -67,35 +156,162 @@ public class MojString {
         * Neka je niz sastavljen od sljedecih znakova "Ovo je neki niz"
         * Ako su funkciji poslani kao parametri 3 i 9 funkcija treba da vrati string " je nek"
         * */
-        return "";
+        String rezultat = "";
+        for(int i = pocetniIndeks; i <= krajnjiIndeks; i++){
+            rezultat += this.niz[i];
+
+        }
+        return rezultat;
     }
 
     public String longestWord(){
         //Funkcija kao rezultat vraca rijec koja je najduza u stringu
         //rijec posmatramo kao bilo koju kombinaciju slova i brojeva, npr.
         // asd, as123dada, 123 su 3 rijeci.
-        return "";
+        String rezultat = "";
+        String rezultatx = "";
+        int i = 0;
+        int j = 0;
+        int min = 0;
+        int currentLength;
+        int maxLength = 0;
+        int maxStartIndex = 0;
+        while(i < this.niz.length){
+
+            while(i < this.niz.length && jeLiSlovo(this.niz[i])){
+                rezultat += this.niz[i];
+                i++;
+            }
+            rezultat += " ";
+
+            while(i < this.niz.length && !jeLiSlovo(this.niz[i])){
+                i++;
+            }
+        }
+        while(j < rezultat.length()){
+            if(j < rezultat.length() && rezultat.charAt(j) != ' '){
+                j++;
+            }
+            else{
+                currentLength = j-min;
+                if(currentLength > maxLength){
+                    maxLength = j;
+                    maxStartIndex = min;
+                }
+                j++;
+                min = j;
+            }
+
+        }
+        for(i = maxStartIndex; i < maxLength; i++){
+
+            rezultatx += rezultat.charAt(i);
+
+        }
+
+        return rezultatx;
     }
 
     public String shortestWord(){
         //Funkcija kao rezultat vraca rijec koja je najkraca u stringu
         //rijec posmatramo kao bilo koju kombinaciju slova i brojeva, npr.
         // asd, as123dada, 123 su 3 rijeci.
-        return "";
+        String rezultat = "";
+        String rezultatx = "";
+        int i = 0;
+        int j = 0;
+        int min = 0;
+        int currentLength;
+        int minLength;
+        int minStartIndex = 0;
+        while(i < this.niz.length){
+
+            while(i < this.niz.length && jeLiSlovo(this.niz[i])){
+                rezultat += this.niz[i];
+                i++;
+            }
+            rezultat += " ";
+
+            while(i < this.niz.length && !jeLiSlovo(this.niz[i])){
+                i++;
+            }
+        }
+        minLength = rezultat.length();
+        while(j < rezultat.length()){
+            if(j < rezultat.length() && rezultat.charAt(j) != ' '){
+                j++;
+            }
+            else{
+                currentLength = j-min;
+                if(currentLength < minLength){
+                    minLength = currentLength;
+                    minStartIndex = min;
+                }
+                j++;
+                min = j;
+            }
+
+        }
+        for(i = minStartIndex; i < (minLength + minStartIndex); i++){
+
+            rezultatx += rezultat.charAt(i);
+
+        }
+
+        return rezultatx;
     }
 
-    public MojString izbaciString(String uljez){
+
+
+    public MojString izbaciString(String uljez) {
         //Funkcija treba da izbaci iz stringa(tj. niza znakova) tekst koji je poslan funkciji kao parametar
-        return this;
-    }
+
+        char nizs[] = new char[this.niz.length];
+        int brEl = 0;
+        int brEl1 = 0;
+        for (int i = 0; i < this.niz.length; i++) {
+            if(this.niz[i] != uljez.charAt(brEl1)){
+                nizs[brEl] = this.niz[i];
+                brEl++;
+
+            }else if (this.niz[i] == uljez.charAt(i)){
+            for (int j = 0; j < uljez.length(); j++) {
+                if(i < uljez.length()-1){
+            if (this.niz[i] == uljez.charAt(j)) {
+
+                    i++;
+                }
+            }
+            }
+            }
+        }
+        this.niz = nizs;
+        return this;}
 
     public MojString replace(char oldChar, char newChar){
         //Funkcija treba da sve oldChar u stringu zamijeni sa newChar-ovima
+        for(int i = 0; i < this.niz.length; i++){
+            if(this.niz[i] == oldChar){
+                this.niz[i] = newChar;
+            }
+        }
+
         return this;
     }
 
-    public boolean equals(MojString nekiString){
+    public boolean equals(String nekiString){
         //funkcija treba da vidi da li je ovaj string i string poslan kao parametar isti!
+
+        char niz[] = new char[nekiString.length()];
+        for(int i = 0; i < niz.length; i++){
+            niz[i] = nekiString.charAt(i);
+        }
+
+        for(int i = 0; i < this.niz.length; i++){
+            if(this.niz[i] != niz[i]){
+                return false;
+                }
+        }
         return true;
     }
 
